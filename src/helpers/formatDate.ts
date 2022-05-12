6c3091a68a3a188ec9152dcd1ece673be1012697
Thien Nguyen <thien.nguyen.cs2704@gmail.com>
2022-05-12 08:43:48 +0700

    fix month

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,7 +1,7 @@
 export const formatDate = (data: string) => {
   const dateObject = new Date(data);
   const day = dateObject.getUTCDate();
-  const month = dateObject.getUTCMonth();
+  const month = dateObject.getUTCMonth() + 1;
   const year = dateObject.getUTCFullYear();
 
   return `${day}-${month}-${year}`;
